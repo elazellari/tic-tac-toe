701f377ce8323661d1a78bd7b5d22c9485566586
Erjon Lazellari <elazellari@myseneca.ca>
2025-04-30 23:10:06 -0400

    Added change Turns logic

--- script.js
+++ script.js
@@ -8,3 +8,30 @@ function Gameboard() {
   }
   return board;
 }
+
+//players object
+
+let players = [
+  {
+    name: "Player One",
+    token: "X",
+  },
+
+  {
+    name: "Player Two",
+    token: "O",
+  },
+];
+
+// logic to switch turns
+let currentPlayer = players[0];
+
+let changeTurn = () => {
+  currentPlayer = currentPlayer === players[0] ? players[1] : players[0];
+};
+
+//testing turns switch
+function display() {
+  console.log(currentPlayer.name);
+  changeTurn();
+}
