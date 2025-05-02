a1bcdd5d819f55cfb01a8afdc2dc178fa5386eef
Erjon Lazellari <elazellari@myseneca.ca>
2025-05-01 22:15:51 -0400

    Added addToken function that fills the board

--- script.js
+++ script.js
@@ -27,3 +27,19 @@ function changeTurn() {
   return (currentPlayer =
     currentPlayer === players[0] ? players[1] : players[0]);
 }
+
+//test function to fill board
+function addToken(mark) {
+  if (mark === 1) {
+    board[0][0] = 1;
+    console.log(board);
+  } else if (mark === 2) {
+    console.log(board);
+    board[0][1] = 2;
+  } else if (mark === 3) {
+    console.log(board);
+    board[0][2] = 3;
+  } else {
+    console.log("Invalid");
+  }
+}
