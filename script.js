7bc1a1a3303cc413fdfcee2059334f15f0d36c73
Erjon Lazellari <elazellari@myseneca.ca>
2025-05-01 22:10:12 -0400

    Added the game initial state and players turns

--- script.js
+++ script.js
@@ -1,12 +1,11 @@
-//Gameboard function
-function Gameboard() {
-  let board = [];
-  let rows = [];
-  for (let i = 0; i < 3; i++) {
-    rows[i] = null;
-    board.push(rows);
-  }
-  return board;
+//draw board
+let board = [];
+let rows = [];
+let columns = 3;
+
+for (let i = 0; i < columns; i++) {
+  rows = [null, null, null];
+  board.push(rows);
 }
 
 //players object
@@ -16,22 +15,15 @@ let players = [
     name: "Player One",
     token: "X",
   },
-
   {
     name: "Player Two",
     token: "O",
   },
 ];
 
-// logic to switch turns
 let currentPlayer = players[0];
-
-let changeTurn = () => {
-  currentPlayer = currentPlayer === players[0] ? players[1] : players[0];
-};
-
-//testing turns switch
-function display() {
-  console.log(currentPlayer.name);
-  changeTurn();
+//function to switch turns between players
+function changeTurn() {
+  return (currentPlayer =
+    currentPlayer === players[0] ? players[1] : players[0]);
 }
